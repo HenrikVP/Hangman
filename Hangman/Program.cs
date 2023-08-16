ec338babf4fec8c420b9462e7f684dee931af125
HenrikVP <hpo@tec.dk>
2023-08-16 10:29:47 +0200

    final

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -2,16 +2,25 @@
 {
     internal class Program
     {
-        static string guessedLetters ="";
+        static int livesLeft = 6;
+        static string guessedLetters = "";
         //\r\\r\nBlyant\r\nVindmølle\r\nChokolade\r\nRegnbue\r\nSkjorte\r\nTelefon\r\nBjergtop\r\nFuglesang\r\nLommelygte\r\nCykel\r\nHavbølge\r\nStjerneskud\r\nPandekage\r\nMikroskop\r\nLænestol\r\nRaket\r\nKaktus
+
+        //TODO wrong answer should cost a life.
+        //TODO Finish game when all letters have been guessed.
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, and welcome to hangman");
             string word = RandomWord();
-            string letter = InputLetter();
-            AddLetterToGuessed(letter);
-            GUI(word);
+
+            while (true)
+            {
+                GUI(word);
+                string letter = InputLetter();
+                AddLetterToGuessed(letter);
+                IsLetterInWord(word, letter);
+            }
         }
 
         static string InputLetter()
@@ -19,12 +28,29 @@
             char letter;
             do
             {
-                Console.Write("\nInput a letter: ");
-                letter = Console.ReadKey().KeyChar;
+                Console.Write("\n\nInput a letter: ");
+                letter = char.ToUpper(Console.ReadKey().KeyChar);
             }
             while (!char.IsLetter(letter));
 
             return letter.ToString();
+        }
+
+        static void IsLetterInWord(string word, string letter)
+        {
+            if (word.Contains(letter)) Console.WriteLine("\nGood job!");
+            else
+            {
+                Console.WriteLine("\nWRONG!!!");
+                Hangman();
+            }
+        }
+
+        static void Hangman()
+        {
+            livesLeft--;
+            if (livesLeft == 0) { Console.WriteLine("You killed a man!"); }
+            //TODO finish the game, ask for a retry
         }
 
         static void AddLetterToGuessed(string letter)
@@ -36,34 +62,16 @@
             else { guessedLetters += letter; }
         }
 
-        static void TestingKeyboard()
-        {
-            while (true)
-            {
-                ConsoleKeyInfo letter = Console.ReadKey();
-                Console.WriteLine(letter + " " + (int)letter.KeyChar);
-            }
-        }
-
         static void GUI(string word)
         {
             //If letter is in position in a loop of char in words,
             //then write the letter otherwise write underscore
-            //int numLetters = word.Length;
-            foreach (char letter in word) 
+            Console.WriteLine($"\nLives Left: {livesLeft}\nGuessed Letters: {guessedLetters}\n");
+            foreach (char letter in word)
             {
-                if (guessedLetters.Contains(letter)) Console.Write(letter);
-                else Console.Write("_");
+                if (guessedLetters.Contains(letter)) Console.Write(letter + " ");
+                else Console.Write("_ ");
             }
-            
-            //for (int i = 0; i < numLetters; i++)
-            //{
-            //    if (guessedLetters.Contains(word[i])) Console.Write(word[i]);
-            //    else Console.Write("_");
-            //}
-
-            //string underScores = "".PadLeft(numLetters, '_');
-            //Console.WriteLine(underScores);
         }
 
         static string RandomWord()
@@ -75,7 +83,7 @@
             Random rnd = new Random();
             int num = rnd.Next(words.Length);
             string word = words[num];
-            return word;
+            return word.ToUpper();
         }
     }
 }
